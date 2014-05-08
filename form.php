c7bc22a543465573d580be50b0c1c42ef3459c67
fr-matsuo <matsuo@fact-real.com>
2014-05-08 08:33:58 +0000

    動作変更を伴わないコード整理 #23

--- form.php
+++ form.php
@@ -20,83 +20,82 @@
         <legend>フォーム</legend>
         
         <label>名前:</label>
-        <input type="text" name="name_first" id="name_first" value="<?php printf('%s', $_POST['name_first']); ?>">
-        <input type="text" name="name_last" id="name_last" value="<?php printf('%s', $_POST['name_last']); ?>">
+        <input type="text" name="name_first" id="name_first" value="<?php print $_POST['name_first']; ?>">
+        <input type="text" name="name_last" id="name_last" value="<?php print $_POST['name_last']; ?>">
         <br> 
         
         <label>性別:</label>
-        <?php $manChecked   = ($_POST['sex'] == "男性")? "checked" : "";?>
-        <?php $womanChecked = ($_POST['sex'] == "女性")? "checked" : "";?>
-        <input type="radio" name="sex" id="man" value="男性" <?php print $manChecked ?>><label for="man">男性</label>
-        <input type="radio" name="sex" id="woman" value="女性" <?php print $womanChecked ?>><label for="woman">女性</label>
+          <?php
+          $manChecked   = ($_POST['sex'] == "男性")? "checked" : "";
+          $womanChecked = ($_POST['sex'] == "女性")? "checked" : "";
+          ?>
+        <input type="radio" name="sex" id="man" value="男性" <?php print $manChecked; ?>><label for="man">男性</label>
+        <input type="radio" name="sex" id="woman" value="女性" <?php print $womanChecked; ?>><label for="woman">女性</label>
         <br>
   
         <label>郵便番号:</label>
-        <input type="text" name="post_first" id="post_first" value="<?php printf('%s', $_POST['post_first']); ?>">
+        <input type="text" name="post_first" id="post_first" value="<?php print $_POST['post_first']; ?>">
         -
-        <input type="text" name="post_last" id="post_last" value="<?php printf('%s', $_POST['post_last']); ?>">
+        <input type="text" name="post_last" id="post_last" value="<?php print $_POST['post_last']; ?>">
         <br>
         
         <label>都道府県:</label>
         
-        <select name="prefecture" id="prefecture" size=1 value="<?php printf('%s', $_POST['prefecture']); ?>">
-          <option id="--" value="--">--</option>
-          <?php
-            $PREFECTURES = array(
-              '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県',
-              '茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県',
-              '新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県',
-              '三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県',
-              '鳥取県','島根県','岡山県','広島県','山口県',
-              '徳島県','香川県','愛媛県','高知県',
-              '福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県'
-            );
+        <select name="prefecture" id="prefecture" size=1 value="<?php print $_POST['prefecture']; ?>">
+        <?php
+        $PREFECTURES = array(
+            '--',
+            '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県',
+            '茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県',
+            '新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県',
+            '三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県',
+            '鳥取県','島根県','岡山県','広島県','山口県',
+            '徳島県','香川県','愛媛県','高知県',
+            '福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県'
+        );
 
-            foreach ($PREFECTURES as $key => $elm) {
-              $selected = ($_POST['prefecture'] == $elm) ? 'selected' : '';
-              printf("<option value='%s' %s>%s</option>", $elm, $selected, $elm);
-            }
-          ?>
-          
-          <!--
-          <option id="Tokyo" value="東京都">東京都</option>
-          <option id="Saitama" value="埼玉県">埼玉県</option>
-          <option id="Gunma" value="群馬県">群馬県</option>
-          -->
+        foreach ($PREFECTURES as $elm) {
+            $selected = ($_POST['prefecture'] == $elm) ? 'selected' : '';
+            printf("<option value='%s' %s>%s</option>", $elm, $selected, $elm);
+        }
+        ?>
         </select>
         <br>
         
         <label>メールアドレス:</label>
-        <input type="text" name="mail_address" id="mail" value="<?php printf('%s', $_POST['mail_address']); ?>">
+        <input type="text" name="mail_address" id="mail" value="<?php print $_POST['mail_address']; ?>">
         <br>
         
         <label>趣味</label>
         <?php
-          $HOBBYS = array('music' => '音楽鑑賞','movie' => '映画鑑賞','other' => 'その他');
-
-          $checkList = $_POST['hobby'];
-
-          foreach ($HOBBYS as $key => $elm) {
+        $HOBBYS = array('music' => '音楽鑑賞','movie' => '映画鑑賞','other' => 'その他');
+  
+        $checkList = $_POST['hobby'];
+  
+        foreach ($HOBBYS as $key => $elm) {
             $checked = (in_array($elm, $checkList)) ? 'checked' : '';
             printf("<input type='checkbox' id='%s' name='hobby[]' value='%s' %s><label for='%s'>%s</label>",
-                    $key, $elm, $checked, $key, $elm);
-          }
+                          $key, $elm, $checked, $key, $elm);
+        }
         ?>
-        <!--
-        <input type="checkbox" name="hobby[]" id="music" value="音楽鑑賞"><label for="music">音楽鑑賞</label>
-        <input type="checkbox" name="hobby[]" id="movie" value="映画鑑賞"><label for="movie">映画鑑賞</label>
-        <input type="checkbox" name="hobby[]" id="other" value="その他"><label for="other">その他</label>
-        -->
-        <input type="text" name="other_descript" id="other_descript" value="<?php printf('%s', $_POST['other_descript']); ?>">
+        <input type="text" name="other_descript" id="other_descript" value="<?php print $_POST['other_descript']; ?>">
         <br>
         
         <label>ご意見</label>
-        <input type="text" id="opinion" name="opinion" value="<?php printf('%s', $_POST['opinion']); ?>">
+        <input type="text" id="opinion" name="opinion" value="<?php print $_POST['opinion']; ?>">
         <br>
   
         <input type="submit" value="確認">
       </fieldset>
     </form>
+    <?php
+    /*
+        foreach ($_POST['errors'] as error) {
+            $error -> show();
+            printf("%s",'<br>');
+        }
+    */
+    ?>
   </section>
 
   <footer>
--- formCheck.php
+++ formCheck.php
@@ -1,99 +1,99 @@
 <?php
-    /*
-    $ERROR_MESSAGE_NO_FIRST_NAME        = "姓を入力してください。";
-    $ERROR_MESSAGE_NO_LAST_NAME         = "名を入力してください。";
-    $ERROR_MESSAGE_OVER_FIRST_NAME      = "姓は50字以内で入力してください。";
-    $ERROR_MESSAGE_OVER_LAST_NAME       = "名は50字以内で入力してください。";
-    $ERROR_MESSAGE_NO_SEX               = "性別を選択してください。";
-    $ERROR_MESSAGE_NO_POST_NUMBER       = "郵便番号を入力してください。";
-    $ERROR_MESSAGE_NO_PREFECTURE        = "都道府県を入力してください。";
-    $ERROR_MESSAGE_NO_MAIL_ADDRESS      = "メールアドレスを入力してください。";
-    $ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS = "メールアドレスを正しく入力してください。";
-    $ERROR_MESSAGE_NO_OTHER             = "その他の詳細を入力してください。";
-    */
+/*
+$ERROR_MESSAGE_NO_FIRST_NAME        = "姓を入力してください。";
+$ERROR_MESSAGE_NO_LAST_NAME         = "名を入力してください。";
+$ERROR_MESSAGE_OVER_FIRST_NAME      = "姓は50字以内で入力してください。";
+$ERROR_MESSAGE_OVER_LAST_NAME       = "名は50字以内で入力してください。";
+$ERROR_MESSAGE_NO_SEX               = "性別を選択してください。";
+$ERROR_MESSAGE_NO_POST_NUMBER       = "郵便番号を入力してください。";
+$ERROR_MESSAGE_NO_PREFECTURE        = "都道府県を入力してください。";
+$ERROR_MESSAGE_NO_MAIL_ADDRESS      = "メールアドレスを入力してください。";
+$ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS = "メールアドレスを正しく入力してください。";
+$ERROR_MESSAGE_NO_OTHER             = "その他の詳細を入力してください。";
+*/
 
-    //エラーメッセージの内容や対象を保持・出力するクラス
-    class ErrorInfo 
-    {
-        //エラー項目の名前
-        public static $NAME = array(
-            'name_first'     => '姓',
-            'name_last'      => '名',
-            'sex'            => '性別',
-            'post'           => '郵便番号',
-            'prefecture'     => '都道府県',
-            'mail_address'   => 'メールアドレス',
-            'other_descript' => 'その他の詳細'
-        );
+//エラーメッセージの内容や対象を保持・出力するクラス
+class ErrorInfo 
+{
+    //エラー項目の名前
+    public static $NAME = array(
+        'name_first'     => '姓',
+        'name_last'      => '名',
+        'sex'            => '性別',
+        'post'           => '郵便番号',
+        'prefecture'     => '都道府県',
+        'mail_address'   => 'メールアドレス',
+        'other_descript' => 'その他の詳細'
+    );
     
-        //エラーの内容
-        public static $KIND = array(
-            'noText'   => '入力',
-            'noChoise' => '選択',
-            'overText' => '字以内で入力',
-            'illegal'  => '正しく入力'
-        );
+    //エラーの内容
+    public static $KIND = array(
+        'noText'   => '入力',
+        'noChoise' => '選択',
+        'overText' => '字以内で入力',
+        'illegal'  => '正しく入力'
+    );
 
-        private $name;  //エラー項目
-        private $kind;  //エラー内容
-        private $value; //'50'字以内などの値
+    private $name;  //エラー項目
+    private $kind;  //エラー内容
+    private $value; //'50'字以内などの値
 
-        public function __construct($errorName, $errorKind, $errorValue) {
-            $this -> name  = ErrorInfo::$NAME[$errorName];
-            $this -> kind  = ErrorInfo::$KIND[$errorKind];
-            $this -> value = $errorValue;
-        }
-
-        //エラー内容を表示
-        public function show(){
-            printf('%sを%s%sしてください。', $this -> name, $this -> value, $this -> kind);
-        }
+    public function __construct($errorName, $errorKind, $errorValue) {
+        $this->name  = ErrorInfo::$NAME[$errorName];
+        $this->kind  = ErrorInfo::$KIND[$errorKind];
+        $this->value = $errorValue;
     }
 
-    //エラー一覧
-    $ERROR_MESSAGE_NO_FIRST_NAME        = new ErrorInfo('name_first',     'noText',   '');
-    $ERROR_MESSAGE_NO_LAST_NAME         = new ErrorInfo('name_last' ,     'noText',   '');
-    $ERROR_MESSAGE_OVER_FIRST_NAME      = new ErrorInfo('name_first',     'overText', '50');
-    $ERROR_MESSAGE_OVER_LAST_NAME       = new ErrorInfo('name_last' ,     'overText', '50');
-    $ERROR_MESSAGE_NO_SEX               = new ErrorInfo('sex',            'noText',   '');
-    $ERROR_MESSAGE_NO_POST_NUMBER       = new ErrorInfo('post',           'noText',   '');
-    $ERROR_MESSAGE_NO_PREFECTURE        = new ErrorInfo('prefecture',     'noText',   '');
-    $ERROR_MESSAGE_NO_MAIL_ADDRESS      = new ErrorInfo('mail_address',   'noText',   '');
-    $ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS = new ErrorInfo('mail_address',   'illegal',  '');
-    $ERROR_MESSAGE_NO_OTHER             = new ErrorInfo('other_descript', 'noText',   '');
+    //エラー内容を表示
+    public function show(){
+        printf('%sを%s%sしてください。', $this->name, $this->value, $this->kind);
+    }
+}
 
-    //_POSTから前後の空白を除いたものを取得
-    function getTrimedPOST() {
-        $trimedArray = array();
+//エラー一覧
+$ERROR_MESSAGE_NO_FIRST_NAME        = new ErrorInfo('name_first',     'noText',   '');
+$ERROR_MESSAGE_NO_LAST_NAME         = new ErrorInfo('name_last' ,     'noText',   '');
+$ERROR_MESSAGE_OVER_FIRST_NAME      = new ErrorInfo('name_first',     'overText', '50');
+$ERROR_MESSAGE_OVER_LAST_NAME       = new ErrorInfo('name_last' ,     'overText', '50');
+$ERROR_MESSAGE_NO_SEX               = new ErrorInfo('sex',            'noText',   '');
+$ERROR_MESSAGE_NO_POST_NUMBER       = new ErrorInfo('post',           'noText',   '');
+$ERROR_MESSAGE_NO_PREFECTURE        = new ErrorInfo('prefecture',     'noText',   '');
+$ERROR_MESSAGE_NO_MAIL_ADDRESS      = new ErrorInfo('mail_address',   'noText',   '');
+$ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS = new ErrorInfo('mail_address',   'illegal',  '');
+$ERROR_MESSAGE_NO_OTHER             = new ErrorInfo('other_descript', 'noText',   '');
 
-        foreach ($_POST as $key => $value) {
-            if (is_array($value)) {
-                foreach ($value as $arrayKey => $arrayValue) {
-                    $trimedArray[$key][$arrayKey] = trim($arrayValue);
-                }
-            } else {
-                $trimedArray[$key] = trim($value);
+//_POSTから前後の空白を除いたものを取得
+function getTrimedPOST() {
+    $trimedArray = array();
+
+    foreach ($_POST as $key => $value) {
+        if (is_array($value)) {
+            foreach ($value as $arrayKey => $arrayValue) {
+                $trimedArray[$key][$arrayKey] = trim($arrayValue);
             }
+        } else {
+            $trimedArray[$key] = trim($value);
         }
+    }
 
-        return $trimedArray;
+    return $trimedArray;
+}
+//エラーメッセージ配列をすべて出力
+function outErrorMessage($errorMessages) {
+    foreach($errorMessages as $msg) {
+        $msg -> show();
     }
-    //エラーメッセージ配列をすべて出力
-    function outErrorMessage($errorMessages) {
-        foreach($errorMessages as $msg) {
-            $msg -> show();
-        }
-    }
+}
     
-    //メールアドレスの書式チェック
-    function isMailAddress($address) {
-        if (substr_count($address, '@') != 1) return false;
-        if (substr_count($address, ' ') != 0) return false;
-        if (strstr($address, '@')       == '@') return false;
-        if (strstr($address, '@', true) == '')  return false;
+//メールアドレスの書式チェック
+function isMailAddress($address) {
+    if (substr_count($address, '@') != 1) return false;
+    if (substr_count($address, ' ') != 0) return false;
+    if (strstr($address, '@')       == '@') return false;
+    if (strstr($address, '@', true) == '')  return false;
         
-        return true;
-    }
+    return true;
+}
 
 ?>
 <!DOCTYPE html>
@@ -113,119 +113,117 @@
 
   <section>
     <form/>
-      <?php
-      ?>
       <p>
         名前：
         <?php
-          $nameErrors = array();
+        $nameErrors = array();
 
-          if (empty($_POST['name_first']))          {array_push($nameErrors, $ERROR_MESSAGE_NO_FIRST_NAME   );}
-          if (empty($_POST['name_last' ]))          {array_push($nameErrors, $ERROR_MESSAGE_NO_LAST_NAME    );}
-          if (mb_strlen($_POST['name_first']) > 50) {array_push($nameErrors, $ERROR_MESSAGE_OVER_FIRST_NAME );}
-          if (mb_strlen($_POST['name_last' ]) > 50) {array_push($nameErrors, $ERROR_MESSAGE_OVER_LAST_NAME  );}
+        if (empty($_POST['name_first']))          {array_push($nameErrors, $ERROR_MESSAGE_NO_FIRST_NAME   );}
+        if (empty($_POST['name_last' ]))          {array_push($nameErrors, $ERROR_MESSAGE_NO_LAST_NAME    );}
+        if (mb_strlen($_POST['name_first']) > 50) {array_push($nameErrors, $ERROR_MESSAGE_OVER_FIRST_NAME );}
+        if (mb_strlen($_POST['name_last' ]) > 50) {array_push($nameErrors, $ERROR_MESSAGE_OVER_LAST_NAME  );}
 
-          //表示
-          if (empty($nameErrors)) {
+        //表示
+        if (empty($nameErrors)) {
             printf("%s %s", $_POST['name_first'] ,$_POST['name_last']);
-          } else {
+        } else {
             outErrorMessage($nameErrors);
-          } 
+        } 
         ?>
       </p>
       <p>
         性別：
         <?php
-          $sexErrors = array();
+        $sexErrors = array();
 
-          if (empty($_POST['sex'])) {array_push($sexErrors, $ERROR_MESSAGE_NO_SEX);}
+        if (empty($_POST['sex'])) {array_push($sexErrors, $ERROR_MESSAGE_NO_SEX);}
 
-          $errorNum = count($sexErrors);
+        $errorNum = count($sexErrors);
 
-          //表示 
-          if ($errorNum == 0) {
-            printf("%s", $_POST['sex']);
-          } else {
+        //表示 
+        if ($errorNum == 0) {
+            print $_POST['sex'];
+        } else {
             outErrorMessage($sexErrors);
-          }
+        }
         ?>
       </p>
        
       <p>
         郵便番号:
         <?php
-          $postErrors = array();
+        $postErrors = array();
 
-          if (empty($_POST['post_first']) || empty($_POST['post_last'])) {array_push($postErrors, $ERROR_MESSAGE_NO_POST_NUMBER);}
+        if (empty($_POST['post_first']) || empty($_POST['post_last'])) {array_push($postErrors, $ERROR_MESSAGE_NO_POST_NUMBER);}
 
-          if (empty($postErrors)) {
+        if (empty($postErrors)) {
             printf("%s-%s", $_POST['post_first'], $_POST['post_last']);
-          } else {
+        } else {
             outErrorMessage($postErrors);
-          }
+        }
         ?>
       </p>
       
       <p>
         都道府県:
         <?php
-          $prefectureErrors = array();
+        $prefectureErrors = array();
           
-          if ($_POST['prefecture'] == '--') {array_push($prefectureErrors, $ERROR_MESSAGE_NO_PREFECTURE);}
+        if ($_POST['prefecture'] == '--') {array_push($prefectureErrors, $ERROR_MESSAGE_NO_PREFECTURE);}
 
-          if (empty($prefectureErrors)) {
+        if (empty($prefectureErrors)) {
             printf("%s", $_POST['prefecture']);
-          } else {
+        } else {
             outErrorMessage($prefectureErrors);
-          }
+        }
         ?>
       </p>
       
       <p>
         メールアドレス:
         <?php
-          $mailErrors = array();
+        $mailErrors = array();
 
-          if (empty($_POST['mail_address'])) {
+        if (empty($_POST['mail_address'])) {
             array_push($mailErrors, $ERROR_MESSAGE_NO_MAIL_ADDRESS);
-          } else if (isMailAddress($_POST['mail_address']) == false) {
+        } else if (isMailAddress($_POST['mail_address']) == false) {
             array_push($mailErrors, $ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS);
-          }
+        }
 
-          if (empty($mailErrors)) {
+        if (empty($mailErrors)) {
             printf("%s", $_POST['mail_address']);
-          } else {
+        } else {
             outErrorMessage($mailErrors);
-          }
+        }
         ?>
       </p>
       
       <p>
         趣味:
         <?php
-          $hobbyErrors = array();
+        $hobbyErrors = array();
 
-          //チェックしたボックス一覧を取得・表示
-          if (isSet($_POST['hobby'])) {
+        //チェックしたボックス一覧を取得・表示
+        if (isSet($_POST['hobby'])) {
             $checkList = $_POST['hobby'];
             $length    = count($checkList);
             if (in_array('その他', $checkList) && empty($_POST['other_descript'])) {
-              array_push($hobbyErrors, $ERROR_MESSAGE_NO_OTHER);
+                  array_push($hobbyErrors, $ERROR_MESSAGE_NO_OTHER);
             }
 
             if (empty($hobbyErrors)) {
-              //表示
-              for ($i = 0; $i < $length-1; $i++ ) {
-                printf("%s,", $checkList[$i]);
-              }
-              printf("%s", $checkList[$length-1]);
-              if (in_array('その他', $checkList)) {
-                printf("(%s)", $_POST['other_descript']);
-              }
+                //表示
+                for ($i = 0; $i < $length-1; $i++ ) {
+                    printf("%s,", $checkList[$i]);
+                }
+                printf("%s", $checkList[$length-1]);
+                if (in_array('その他', $checkList)) {
+                    printf("(%s)", $_POST['other_descript']);
+                }
             } else {
-              outErrorMessage($hobbyErrors);
+                outErrorMessage($hobbyErrors);
             }
-          }
+        }
         ?>
       </p>
       
@@ -248,11 +246,11 @@
       <input type='hidden' name='prefecture'     value="<?php printf('%s', $_POST['prefecture']);    ?>">
       <input type='hidden' name='mail_address'   value="<?php printf('%s', $_POST['mail_address']);  ?>">
       <?php
-        if(empty($checkList) == false) {
+      if(empty($checkList) == false) {
           for ($i = 0; $i < $length; $i++) {
-            printf("<input type='hidden' name='hobby[]' value='%s'>", $checkList[$i]);
+              printf("<input type='hidden' name='hobby[]' value='%s'>", $checkList[$i]);
           }
-        }
+      }
       ?>
       <input type='hidden' name='other_descript' value="<?php printf('%s', $_POST['other_descript']);?>">
       <input type='hidden' name='opinion'        value="<?php printf('%s', $_POST['opinion']);       ?>">

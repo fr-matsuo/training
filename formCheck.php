8d42d63fcd6697f1dc4ad24b9ff080fcfaa39332
fr-matsuo <matsuo@fact-real.com>
2014-05-09 02:01:52 +0000

    エラー情報の生成をErrorInfoに委託 #23

--- formCheck.php
+++ formCheck.php
@@ -1,22 +1,9 @@
 <?php
-/*
-$ERROR_MESSAGE_NO_FIRST_NAME        = "姓を入力してください。";
-$ERROR_MESSAGE_NO_LAST_NAME         = "名を入力してください。";
-$ERROR_MESSAGE_OVER_FIRST_NAME      = "姓は50字以内で入力してください。";
-$ERROR_MESSAGE_OVER_LAST_NAME       = "名は50字以内で入力してください。";
-$ERROR_MESSAGE_NO_SEX               = "性別を選択してください。";
-$ERROR_MESSAGE_NO_POST_NUMBER       = "郵便番号を入力してください。";
-$ERROR_MESSAGE_NO_PREFECTURE        = "都道府県を入力してください。";
-$ERROR_MESSAGE_NO_MAIL_ADDRESS      = "メールアドレスを入力してください。";
-$ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS = "メールアドレスを正しく入力してください。";
-$ERROR_MESSAGE_NO_OTHER             = "その他の詳細を入力してください。";
-*/
-
 //エラーメッセージの内容や対象を保持・出力するクラス
 class ErrorInfo 
 {
     //エラー項目の名前
-    public static $NAME = array(
+    private static $NAME = array(
         'name_first'     => '姓',
         'name_last'      => '名',
         'sex'            => '性別',
@@ -27,40 +14,67 @@ class ErrorInfo
     );
     
     //エラーの内容
-    public static $KIND = array(
+    private static $KIND = array(
         'noText'   => '入力',
         'noChoise' => '選択',
         'overText' => '字以内で入力',
         'illegal'  => '正しく入力'
     );
 
+    //チェック関数に配列を渡すと、エラーの場合のみインスタンスを追加。インスタンスの生成はこれらのみ
+    
+    public static function checkIsNoText(&$errorArray, $data, $name) {           //必須入力チェック
+        if (empty($data)) {
+            array_push($errorArray, new ErrorInfo($name, 'noText', ''));
+            return true;
+        }
+        return false;
+    }
+    public static function checkIsEmptyValue(&$errorArray, $data, $name, $emptyValue){//必須入力チェック(空を表す文字列と一致しないか)
+        if ($data == $emptyValue) {
+            array_push($errorArray, new ErrorInfo($name, 'noText', ''));
+            return true;
+        }
+        return false;
+    }
+   
+    public static function checkIsNoChoise(&$errorArray, $data, $name) {         //必須選択チェック
+        if (empty($data)) {
+            array_push($errorArray, new ErrorInfo($name, 'noChoise', ''));
+            return true;
+        }
+        return false;
+    }
+    public static function checkIsOverText(&$errorArray, $data, $name, $value) { //字数チェック
+        if (mb_strlen($data) > $value) {
+            array_push($errorArray, new ErrorInfo($name, 'overText', $value));
+            return true;
+        }
+        return false;
+    }
+    public static function checkIsIllegal(&$errorArray, $data, $name) {          //文法チェック
+        if ($name == 'mail_address' && isMailAddress($data) == false) {
+            array_push($errorArray, new ErrorInfo($name, 'illegal', ''));
+            return true;
+        }
+        return false;
+    }
+
     private $name;  //エラー項目
     private $kind;  //エラー内容
     private $value; //'50'字以内などの値
 
-    public function __construct($errorName, $errorKind, $errorValue) {
+    private function __construct($errorName, $errorKind, $errorValue) {
         $this->name  = ErrorInfo::$NAME[$errorName];
         $this->kind  = ErrorInfo::$KIND[$errorKind];
         $this->value = $errorValue;
     }
-
     //エラー内容を表示
-    public function show(){
-        printf('%sを%s%sしてください。', $this->name, $this->value, $this->kind);
+    public function show() {
+        $valueText = (empty($this->value)) ? (string)$this->value : '';
+        printf('%sを%s%sしてください。', $this->name, $valueText, $this->kind);
     }
 }
-
-//エラー一覧
-$ERROR_MESSAGE_NO_FIRST_NAME        = new ErrorInfo('name_first',     'noText',   '');
-$ERROR_MESSAGE_NO_LAST_NAME         = new ErrorInfo('name_last' ,     'noText',   '');
-$ERROR_MESSAGE_OVER_FIRST_NAME      = new ErrorInfo('name_first',     'overText', '50');
-$ERROR_MESSAGE_OVER_LAST_NAME       = new ErrorInfo('name_last' ,     'overText', '50');
-$ERROR_MESSAGE_NO_SEX               = new ErrorInfo('sex',            'noText',   '');
-$ERROR_MESSAGE_NO_POST_NUMBER       = new ErrorInfo('post',           'noText',   '');
-$ERROR_MESSAGE_NO_PREFECTURE        = new ErrorInfo('prefecture',     'noText',   '');
-$ERROR_MESSAGE_NO_MAIL_ADDRESS      = new ErrorInfo('mail_address',   'noText',   '');
-$ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS = new ErrorInfo('mail_address',   'illegal',  '');
-$ERROR_MESSAGE_NO_OTHER             = new ErrorInfo('other_descript', 'noText',   '');
 
 //_POSTから前後の空白を除いたものを取得
 function getTrimedPOST() {
@@ -78,10 +92,11 @@ function getTrimedPOST() {
 
     return $trimedArray;
 }
+
 //エラーメッセージ配列をすべて出力
 function outErrorMessage($errorMessages) {
     foreach($errorMessages as $msg) {
-        $msg -> show();
+        $msg->show();
     }
 }
     
@@ -117,18 +132,18 @@ function isMailAddress($address) {
         名前：
         <?php
         $nameErrors = array();
-
-        if (empty($_POST['name_first']))          {array_push($nameErrors, $ERROR_MESSAGE_NO_FIRST_NAME   );}
-        if (empty($_POST['name_last' ]))          {array_push($nameErrors, $ERROR_MESSAGE_NO_LAST_NAME    );}
-        if (mb_strlen($_POST['name_first']) > 50) {array_push($nameErrors, $ERROR_MESSAGE_OVER_FIRST_NAME );}
-        if (mb_strlen($_POST['name_last' ]) > 50) {array_push($nameErrors, $ERROR_MESSAGE_OVER_LAST_NAME  );}
-
+        
+        ErrorInfo::checkIsNoText($nameErrors, $_POST['name_first'], 'name_first');
+        ErrorInfo::checkIsNoText($nameErrors, $_POST['name_last' ], 'name_first');
+        ErrorInfo::checkIsOverText($nameErrors, $_POST['name_first'], 'name_first', 50);
+        ErrorInfo::checkIsOverText($nameErorrs, $_POST['name_last'] , 'name_last',  50);
+        
         //表示
         if (empty($nameErrors)) {
             printf("%s %s", $_POST['name_first'] ,$_POST['name_last']);
         } else {
             outErrorMessage($nameErrors);
-        } 
+        }
         ?>
       </p>
       <p>
@@ -136,12 +151,10 @@ function isMailAddress($address) {
         <?php
         $sexErrors = array();
 
-        if (empty($_POST['sex'])) {array_push($sexErrors, $ERROR_MESSAGE_NO_SEX);}
-
-        $errorNum = count($sexErrors);
+        ErrorInfo::checkIsNoChoise($sexErrors, $_POST['sex'], 'sex');
 
         //表示 
-        if ($errorNum == 0) {
+        if (empty($sexErrors)) {
             print $_POST['sex'];
         } else {
             outErrorMessage($sexErrors);
@@ -153,8 +166,10 @@ function isMailAddress($address) {
         郵便番号:
         <?php
         $postErrors = array();
-
-        if (empty($_POST['post_first']) || empty($_POST['post_last'])) {array_push($postErrors, $ERROR_MESSAGE_NO_POST_NUMBER);}
+        
+        //前半入力済みの場合のみ後半を調べて、エラーの重複を避ける
+        $isNoFirstNumber = ErrorInfo::checkIsNoText($postErrors, $_POST['post_first'], 'post');
+        if($isNoFirstNumber == false) ErrorInfo::checkIsNoText($postErrors, $_POST['post_last'], 'post');
 
         if (empty($postErrors)) {
             printf("%s-%s", $_POST['post_first'], $_POST['post_last']);
@@ -168,11 +183,11 @@ function isMailAddress($address) {
         都道府県:
         <?php
         $prefectureErrors = array();
-          
-        if ($_POST['prefecture'] == '--') {array_push($prefectureErrors, $ERROR_MESSAGE_NO_PREFECTURE);}
+        
+        ErrorInfo::checkIsEmptyValue($prefectureErrors, $_POST['prefecture'], 'prefecture', '--');
 
         if (empty($prefectureErrors)) {
-            printf("%s", $_POST['prefecture']);
+            print $_POST['prefecture'];
         } else {
             outErrorMessage($prefectureErrors);
         }
@@ -184,20 +199,18 @@ function isMailAddress($address) {
         <?php
         $mailErrors = array();
 
-        if (empty($_POST['mail_address'])) {
-            array_push($mailErrors, $ERROR_MESSAGE_NO_MAIL_ADDRESS);
-        } else if (isMailAddress($_POST['mail_address']) == false) {
-            array_push($mailErrors, $ERROR_MESSAGE_ILLEGAL_MAIL_ADDRESS);
-        }
+        //入力されている場合のみ書式を調べる
+        $noMailAddress = ErrorInfo::checkIsNoText($mailErrors, $_POST['mail_address'], 'mail_address');
+        if ($noMailAddress == false) ErrorInfo::checkIsIllegal($mailErrors, $_POST['mail_address'], 'mail_address');
 
         if (empty($mailErrors)) {
-            printf("%s", $_POST['mail_address']);
+            print $_POST['mail_address'];
         } else {
             outErrorMessage($mailErrors);
         }
         ?>
       </p>
-      
+
       <p>
         趣味:
         <?php
@@ -207,8 +220,10 @@ function isMailAddress($address) {
         if (isSet($_POST['hobby'])) {
             $checkList = $_POST['hobby'];
             $length    = count($checkList);
-            if (in_array('その他', $checkList) && empty($_POST['other_descript'])) {
-                  array_push($hobbyErrors, $ERROR_MESSAGE_NO_OTHER);
+
+            //その他があれば、詳細の入力をチェック
+            if (in_array('その他', $checkList)) {
+                ErrorInfo::checkIsNoText($hobbyErrors, $_POST['other_descript'], 'other_descript');
             }
 
             if (empty($hobbyErrors)) {
@@ -217,6 +232,7 @@ function isMailAddress($address) {
                     printf("%s,", $checkList[$i]);
                 }
                 printf("%s", $checkList[$length-1]);
+                //その他があれば詳細を表示
                 if (in_array('その他', $checkList)) {
                     printf("(%s)", $_POST['other_descript']);
                 }
@@ -229,22 +245,18 @@ function isMailAddress($address) {
       
       <p>
         ご意見:
-        <?php
-          printf("%s", $_POST['opinion']);
-        ?>
+        <?php print $_POST['opinion']; ?>
       </p>
       <input type="submit" value="送信" formaction="finish.php">
     </form>  
-    
-    
     <form action="form.php" method="post">
-      <input type='hidden' name='name_first'     value="<?php printf('%s', $_POST['name_first']);    ?>">
-      <input type='hidden' name='name_last'      value="<?php printf('%s', $_POST['name_last']);     ?>">
-      <input type='hidden' name='sex'            value="<?php printf('%s', $_POST['sex']);           ?>">
-      <input type='hidden' name='post_first'     value="<?php printf('%s', $_POST['post_first']);    ?>">
-      <input type='hidden' name='post_last'      value="<?php printf('%s', $_POST['post_last']);     ?>">
-      <input type='hidden' name='prefecture'     value="<?php printf('%s', $_POST['prefecture']);    ?>">
-      <input type='hidden' name='mail_address'   value="<?php printf('%s', $_POST['mail_address']);  ?>">
+      <input type='hidden' name='name_first'     value="<?php printf('%s', $_POST['name_first']);   ?>">
+      <input type='hidden' name='name_last'      value="<?php printf('%s', $_POST['name_last']);    ?>">
+      <input type='hidden' name='sex'            value="<?php printf('%s', $_POST['sex']);          ?>">
+      <input type='hidden' name='post_first'     value="<?php printf('%s', $_POST['post_first']);   ?>">
+      <input type='hidden' name='post_last'      value="<?php printf('%s', $_POST['post_last']);    ?>">
+      <input type='hidden' name='prefecture'     value="<?php printf('%s', $_POST['prefecture']);   ?>">
+      <input type='hidden' name='mail_address'   value="<?php printf('%s', $_POST['mail_address']); ?>">
       <?php
       if(empty($checkList) == false) {
           for ($i = 0; $i < $length; $i++) {
@@ -252,8 +264,8 @@ function isMailAddress($address) {
           }
       }
       ?>
-      <input type='hidden' name='other_descript' value="<?php printf('%s', $_POST['other_descript']);?>">
-      <input type='hidden' name='opinion'        value="<?php printf('%s', $_POST['opinion']);       ?>">
+      <input type='hidden' name='other_descript' value="<?php printf('%s', $_POST['other_descript']); ?>">
+      <input type='hidden' name='opinion'        value="<?php printf('%s', $_POST['opinion']);        ?>">
        
       <input type='submit' value='戻る'>
     </form>

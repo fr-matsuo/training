8774dced766aee605bf6fd4525d571700429e9aa
fr-matsuo <matsuo@fact-real.com>
2014-05-13 04:11:56 +0000

    エラーメッセージの作成と保持を行うクラスError_Messageを追加 #23

--- form.php
+++ form.php
@@ -1,3 +1,56 @@
+<?php
+{
+    //エラー項目の名前
+    private static $_NAME = array(
+         'name_first'     => '姓',
+         'name_last'      => '名',
+         'sex'            => '性別',
+         'post_first'     => '郵便番号',
+         'post_last'      => '郵便番号',
+         'prefecture'     => '都道府県',
+         'mail_address'   => 'メールアドレス',
+         'other_descript' => 'その他の詳細'
+    );
+    //エラーの内容
+    private static $_KIND = array(
+        'noText'   => '入力',
+        'noChoise' => '選択',
+        'overText' => '字以内で入力',
+        'illegal'  => '正しく入力'
+    );
+     //エラーが一つでもあるか
+    private static $_hasError = false;
+
+    //生成されたエラーのテキストをすべて格納
+    private static $_allErrorString = array();
+    //一つでも生成されたら、エラーがあるのでtrue
+    public static function hasError(){return Error_Message::$_hasError;}
+
+    //全エラーの文字列を取得
+    public static function getAllErrorString(){return Error_Message::$_allErrorString;}
+
+    private $_name;  //エラー項目
+    private $_kind;  //エラー内容
+    private $_value; //'50'字以内などの値
+
+    public function __construct($errorName, $errorKind, $errorValue) {
+        $this->_name  = Error_Message::$_NAME[$errorName];
+        $this->_kind  = Error_Message::$_KIND[$errorKind];
+        $this->_value = $errorValue;
+
+        Error_Message::$_hasError = true;
+
+        $this->_addErrorString();
+    }
+    //このエラーの表示を一覧に追加
+    $valueText = (empty($this->_value)) ? (string)$this->_value : '';
+         $text      = sprintf('%sを%s%sしてください。', $this->_name, $valueText, $this->_kind);
+         array_push(Error_Message::$_allErrorString, $text);
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 
 <html>

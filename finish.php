980825cbed13bd2ec8a5ed5e5856d0a1d38cbf7a
fr-matsuo <matsuo@fact-real.com>
2014-05-26 04:58:01 +0000

    DBへの接続と値の送信をPDO::prepareを使用して実装 #23

--- finish.php
+++ finish.php
@@ -1,5 +1,76 @@
-<!DOCTYPE html>
+<?php
+function sendPOST2DB() {
+    $dsn = 'mysql:dbname=firstDB;host=127.0.0.1';
+    $user = 'root';
+    $pdo  = null;
+    //接続
+    try {
+        $pdo = new PDO($dsn, $user);
+    } catch (PDOException $e) {
+        print('Connection failed:'.$e->getMessage());
+        var_dump($pdo);
+    }
+    //登録
+    try {
+        sendDBModule($pdo);
+    } catch (PDOException $e) {
+        print('Add failed:'.$e->getMessage());
+        var_dump($e->getMessage());
+    }
+    //切断
+    $pdo = null;
+}
 
+function sendDBModule($pdo) {
+    $query = $pdo->prepare("
+        INSERT INTO account_info(
+            first_name,
+            last_name,
+            email,
+            pref_id,
+            created_at,
+            updated_at
+        ) VALUES (
+            :first_name,
+            :last_name,
+            :email,
+            :pref_id,
+            NOW(),
+            NOW()
+        )
+        ");
+
+    $first_name = $_POST['name_first'];
+    $last_name  = $_POST['name_last'];
+    $email      = $_POST['mail_address'];
+    $pref_id    = getPrefectureID($pdo);
+
+    $query->bindParam(':first_name', $first_name);
+    $query->bindParam(':last_name' , $last_name);
+    $query->bindParam(':email'     , $email);
+    $query->bindParam(':pref_id'   , $pref_id);
+
+    $result = $query->execute();
+}
+
+function getPrefectureID($pdo) {
+    $query = $pdo->prepare("
+        SELECT pref_id FROM prefecture_info
+        WHERE pref_name = :pref_name
+        ");
+
+    $pref_name = $_POST['prefecture'];
+    $query->bindParam(':pref_name', $pref_name);
+    $query->execute();
+
+    $pref_id = intval($query->fetch()[0]);
+    $query->closeCursor();
+
+    return $pref_id;
+}
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
@@ -10,13 +81,16 @@
 </head>
 
 <body>
+  <?php
+      sendPOST2DB();
+  ?>
   <header>
     <h1>フォーム>完了</h1>
   </header>
 
   <nav>
     応募しました
-    <p><a href="index.php">TOPページへ</a><p>
+    <p><a href="index.html">TOPページへ</a><p>
   </nav>
 
   <footer>

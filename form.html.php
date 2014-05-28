431a378fc5e23ac787e7eefaa809fe65870b3ed0
fr-matsuo <matsuo@fact-real.com>
2014-05-28 08:45:47 +0000

    html部内のphp部を移動 #33

--- form.html.php
+++ form.html.php
@@ -25,10 +25,6 @@
     <br> 
     
     <label>性別:</label>
-      <?php
-      $man_checked   = (getPOST('sex') == "男性")? "checked" : "";
-      $woman_checked = (getPOST('sex') == "女性")? "checked" : "";
-      ?>
     <input type="radio" name="sex" id="man" value="男性" <?php print $man_checked; ?>><label for="man">男性</label>
     <input type="radio" name="sex" id="woman" value="女性" <?php print $woman_checked; ?>><label for="woman">女性</label>
     <br>
@@ -43,16 +39,6 @@
 
     <select name="prefecture" id="prefecture" size=1 value="<?php showPOST('prefecture'); ?>">
     <?php
-    $PREFECTURES = array(
-        '--',
-        '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県',
-        '茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県',
-        '新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県',
-        '三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県',
-        '鳥取県','島根県','岡山県','広島県','山口県',
-        '徳島県','香川県','愛媛県','高知県',
-        '福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県'
-    );
 
     foreach ($PREFECTURES as $elm) {
         $selected = (getPOST('prefecture') == $elm) ? 'selected' : '';
@@ -68,10 +54,6 @@
 
     <label>趣味</label>
     <?php
-    $HOBBYS = array('music' => '音楽鑑賞','movie' => '映画鑑賞','other' => 'その他');
-
-    $check_list = getPOSTArray('hobby');
-
     foreach ($HOBBYS as $key => $elm) {
         $checked = '';
         
@@ -94,13 +76,6 @@
 
   <form method="post" name="checkForm" action="formCheck.php">
   <?php
-  $formated_post = getFormatedTextArray($_POST);
-
-  $NAMES = array(
-      'name_first', 'name_last', 'sex', 'post_first', 'post_last',
-      'prefecture','mail_address', 'other_descript', 'opinion'
-  );
-
   foreach ($NAMES as $name) {
       $input = (isset($formated_post[$name])) ? $formated_post[$name] : '';
       printf("<input type='hidden' name='%s' value='%s'>", $name, $input);
--- form.php
+++ form.php
@@ -2,6 +2,31 @@
 require_once('error_checker.php');
 require_once('format_text.php');
 
+/*以下表示用*/
+
+$man_checked   = (getPOST('sex') == "男性")? "checked" : "";
+$woman_checked = (getPOST('sex') == "女性")? "checked" : "";
+
+$PREFECTURES = array(
+    '--',
+    '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県',
+    '茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県',
+    '新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県',
+    '三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県',
+    '鳥取県','島根県','岡山県','広島県','山口県',
+    '徳島県','香川県','愛媛県','高知県',
+    '福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県'
+);
+
+$HOBBYS = array('music' => '音楽鑑賞','movie' => '映画鑑賞','other' => 'その他');
+$check_list = getPOSTArray('hobby');
+
+$formated_post = getFormatedTextArray($_POST);
+$NAMES = array(
+    'name_first', 'name_last', 'sex', 'post_first', 'post_last',
+    'prefecture','mail_address', 'other_descript', 'opinion'
+);
+
 //値のエラーをチェックし、エラー一覧を返す
 function checkErrors() {
     $formated_post = getFormatedTextArray($_POST);

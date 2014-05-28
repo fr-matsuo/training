4432e790d19320ea7e1722e9c9e77f52e3c28ed6
fr-matsuo <matsuo@fact-real.com>
2014-05-28 04:25:06 +0000

    php部とhtml部を分割 #33

--- finish.html.php
+++ finish.html.php
@@ -1,24 +1,27 @@
-<!DOCTYPE html>
+<?php require_once('finish.php'); ?>
 
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="Content-Style-Type" content="text/css">
   <link rel="stylesheet" type="text/css" href="common.css">
-  <title>トップページ</title>
+  <link rel="stylesheet" type="text/css" href="finish.css">
+  <title>完了</title>
 </head>
 
 <body>
   <header>
-    <h1>フォーム>TOPページ</h1>
+    <h1>フォーム>完了</h1>
   </header>
 
   <nav>
-    <p><a href="form.html.php">フォームを入力する</a></p>
+    応募しました
+    <p><a href="index.php">TOPページへ</a><p>
   </nav>
 
   <footer>
     <p>Copyright 2014</p>
   </footer>
-</body>	
+</body>
 </html>
--- finish.php
+++ finish.php
@@ -70,30 +70,3 @@ function getPrefectureID($pdo, $pref_name) {
 }
 
 sendPOST2DB($_POST);
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <link rel="stylesheet" type="text/css" href="common.css">
-  <link rel="stylesheet" type="text/css" href="finish.css">
-  <title>完了</title>
-</head>
-
-<body>
-  <header>
-    <h1>フォーム>完了</h1>
-  </header>
-
-  <nav>
-    応募しました
-    <p><a href="index.php">TOPページへ</a><p>
-  </nav>
-
-  <footer>
-    <p>Copyright 2014</p>
-  </footer>
-</body>
-</html>

80cbc6895f75807083daecb3f7d474b5b64488fe
fr-matsuo <matsuo@fact-real.com>
2014-05-29 04:28:26 +0000

    formated_postを関数の引数でとるように修正 #33

--- form.html.php
+++ form.html.php
@@ -9,7 +9,7 @@
   <title>フォーム画面</title>  
 </head>
 
-<body <?php checkJump(); ?> >
+<body <?php checkJump($formated_post); ?> >
 <header>
 <h1>フォーム>入力</h1>
 </header>
@@ -20,8 +20,8 @@
     <legend>フォーム</legend>
 
     <label>名前:</label>
-    <input type="text" name="name_first" id="name_first" value="<?php showPOST('name_first'); ?>">
-    <input type="text" name="name_last" id="name_last" value="<?php showPOST('name_last'); ?>">
+    <input type="text" name="name_first" id="name_first" value="<?php showPOST('name_first', $formated_post); ?>">
+    <input type="text" name="name_last" id="name_last" value="<?php showPOST('name_last', $formated_post); ?>">
     <br> 
     
     <label>性別:</label>
@@ -30,34 +30,34 @@
     <br>
 
     <label>郵便番号:</label>
-    <input type="text" name="post_first" id="post_first" maxlength="3" value="<?php showPOST('post_first'); ?>">
+    <input type="text" name="post_first" id="post_first" maxlength="3" value="<?php showPOST('post_first', $formated_post); ?>">
     -
-    <input type="text" name="post_last" id="post_last" maxlength="4" value="<?php showPOST('post_last'); ?>">
+    <input type="text" name="post_last" id="post_last" maxlength="4" value="<?php showPOST('post_last', $formated_post); ?>">
     <br>
 
     <label>都道府県:</label>
-    <?php showPrefectures(); ?>
+    <?php showPrefectures($formated_post); ?>
     <br>
     
     <label>メールアドレス:</label>
-    <input type="text" name="mail_address" id="mail" value="<?php showPOST('mail_address'); ?>">
+    <input type="text" name="mail_address" id="mail" value="<?php showPOST('mail_address', $formated_post); ?>">
     <br>
 
     <label>趣味</label>
-    <?php showHobbys(); ?>
+    <?php showHobbys($formated_post); ?>
     <br>
 
     <label>ご意見</label>
-    <input type="text" id="opinion" name="opinion" value="<?php showPOST('opinion'); ?>">
+    <input type="text" id="opinion" name="opinion" value="<?php showPOST('opinion', $formated_post); ?>">
 
     <input type="submit" value="確認">
     </fieldset>
   </form>
 
-  <?php showError(); ?>
+  <?php showError($formated_post); ?>
 
   <form method="post" name="checkForm" action="formCheck.php">
-  <?php writeHiddenParams(); ?>
+  <?php writeHiddenParams($formated_post); ?>
   </form>
 
   <footer>
--- form.php
+++ form.php
@@ -4,9 +4,6 @@ require_once('format_text.php');
 
 /*以下表示用*/
 
-$man_checked   = (getPOST('sex') == "男性")? "checked" : "";
-$woman_checked = (getPOST('sex') == "女性")? "checked" : "";
-
 $PREFECTURES = array(
     '--',
     '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県',
@@ -19,32 +16,34 @@ $PREFECTURES = array(
 );
 
 $HOBBYS = array('music' => '音楽鑑賞','movie' => '映画鑑賞','other' => 'その他');
-$check_list = getPOSTArray('hobby');
 
-$formated_post = getFormatedTextArray($_POST);
 $NAMES = array(
     'name_first', 'name_last', 'sex', 'post_first', 'post_last',
     'prefecture','mail_address', 'other_descript', 'opinion'
 );
 
+$formated_post = getFormatedTextArray($_POST);
+
+$man_checked   = (getPOST('sex', $formated_post) == "男性")? "checked" : "";
+$woman_checked = (getPOST('sex', $formated_post) == "女性")? "checked" : "";
+
+$check_list = getPOSTArray('hobby', $formated_post);
+
 //値のエラーをチェックし、エラー一覧を返す
-function checkErrors() {
-    global $formated_post;
-
+function checkErrors($post_data) {
     //エラーチェックの引数リスト作成
-
     $name_check_functions = array(
-        new Check_Function_Data('name_first', $formated_post['name_first'], 'checkIsNoText', 0),
-        new Check_Function_Data('name_last',  $formated_post['name_last'] , 'checkIsNoText', 0),
-        new Check_Function_Data('name_first', $formated_post['name_first'], 'checkIsOverText', 1, 50),
-        new Check_Function_Data('name_last',  $formated_post['name_last'] , 'checkIsOverText', 1, 50)
+        new Check_Function_Data('name_first', $post_data['name_first'], 'checkIsNoText', 0),
+        new Check_Function_Data('name_last',  $post_data['name_last'] , 'checkIsNoText', 0),
+        new Check_Function_Data('name_first', $post_data['name_first'], 'checkIsOverText', 1, 50),
+        new Check_Function_Data('name_last',  $post_data['name_last'] , 'checkIsOverText', 1, 50)
     );
     $name_checker = new Error_Checker(
         '名前',
         $name_check_functions
     );
 
-    $sex_value = isset($formated_post['sex']) ? $formated_post['sex'] : '';
+    $sex_value = isset($post_data['sex']) ? $post_data['sex'] : '';
     $sex_check_functions = array(
         new Check_Function_Data('sex', $sex_value, 'checkIsNoChoise', 0)
     );
@@ -54,10 +53,10 @@ function checkErrors() {
     );
 
     $post_check_functions = array(
-        new Check_Function_Data('post_first', $formated_post['post_first'], 'checkIsNoText', 0),
-        new Check_Function_Data('post_last',  $formated_post['post_last'],  'checkIsNoText', 1),
-        new Check_Function_Data('post_first', $formated_post['post_first'], 'checkIsIllegal', 2),
-        new Check_Function_Data('post_last',  $formated_post['post_last'],  'checkIsIllegal', 3)
+        new Check_Function_Data('post_first', $post_data['post_first'], 'checkIsNoText', 0),
+        new Check_Function_Data('post_last',  $post_data['post_last'],  'checkIsNoText', 1),
+        new Check_Function_Data('post_first', $post_data['post_first'], 'checkIsIllegal', 2),
+        new Check_Function_Data('post_last',  $post_data['post_last'],  'checkIsIllegal', 3)
     );
     $post_checker = new Error_Checker(
         '郵便番号',
@@ -65,7 +64,7 @@ function checkErrors() {
     );
 
     $prefecture_check_functions = array(
-        new Check_Function_Data('prefecture', $formated_post['prefecture'], 'checkIsEmptyValue', 0, '--')
+        new Check_Function_Data('prefecture', $post_data['prefecture'], 'checkIsEmptyValue', 0, '--')
     );
     $prefecture_checker = new Error_Checker(
         '都道府県',
@@ -73,9 +72,9 @@ function checkErrors() {
     );
 
     $mail_address_check_functions = array(
-        new Check_Function_Data('mail_address', $formated_post['mail_address'], 'checkIsNoText',  0),
-        new Check_Function_Data('mail_address', $formated_post['mail_address'], 'checkIsIllegal', 1),
-        new Check_Function_Data('mail_address', $formated_post['mail_address'], 'checkIsOverLap', 2)
+        new Check_Function_Data('mail_address', $post_data['mail_address'], 'checkIsNoText',  0),
+        new Check_Function_Data('mail_address', $post_data['mail_address'], 'checkIsIllegal', 1),
+        new Check_Function_Data('mail_address', $post_data['mail_address'], 'checkIsOverLap', 2)
     );
     $mail_address_checker = new Error_Checker(
         'メールアドレス',
@@ -83,8 +82,8 @@ function checkErrors() {
     );
 
     $hobby_check_functions = array();
-    if (isset($formated_post['hobby']) && in_array('その他', $formated_post['hobby'])) {
-        $other_hobby_value = isset($formated_post['other_descript']) ? $formated_post['other_descript'] : '';
+    if (isset($post_data['hobby']) && in_array('その他', $post_data['hobby'])) {
+        $other_hobby_value = isset($post_data['other_descript']) ? $post_data['other_descript'] : '';
         array_push(
             $hobby_check_functions,
             new Check_Function_Data('other_descript', $other_hobby_value, 'checkIsNoText', 0)
@@ -114,20 +113,18 @@ function checkErrors() {
 }
 
 //次のページに行けるならジャンプする関数。入力をform.phpに戻し、エラーがないならformCheck.phpへジャンプ
-function checkJump() {
-    global $formated_post;
-    if (empty($formated_post) || isset($formated_post['return'])) return;
+function checkJump($post_data) {
+    if (empty($post_data) || isset($post_data['return'])) return;
     
-    checkErrors();
+    checkErrors($post_data);
     if (Error_Message::hasError() == false ) {
         print "onLoad='document.checkForm.submit();'";
     }      
 }
 
 //エラーがあればエラー一覧を表示
-function showError() {
-    global $formated_post;
-    if (empty($formated_post) || isset($formated_post['return'])) return;
+function showError($post_data) {
+    if (empty($post_data) || isset($post_data['return'])) return;
 
     if (Error_Message::hasError()) {
         $error_texts = Error_Message::getAllErrorString();
@@ -139,32 +136,31 @@ function showError() {
     }
 }
 
-//ポストデータがあればその文字列を、なければ空文字を返す
-function getPOST($key) {
-    global $formated_post;
-    return (isset($formated_post[$key])) ? $formated_post[$key] : '';
+//ポストデータがなければ空文字を返す、あればその文字列を返す
+function getPOST($key, $post_data) {
+    return (isset($post_data[$key])) ? $post_data[$key] : '';
 }
 
 //getPOSTの配列版
-function getPOSTArray($key) {
+function getPOSTArray($key, $post_data) {
     $ret = array();
-    if (!isset($_POST[$key]) || !is_array($_POST[$key])) return $ret;
+    if (!isset($post_data[$key]) || !is_array($post_data[$key])) return $ret;
     
-    foreach ($_POST[$key] as $key => $value) {
+    foreach ($post_data[$key] as $key => $value) {
         $ret += array($key => getFormatedText($value,0));
     }
     return $ret;
 }
 
 //ポストの値があれば表示、なければ空白を表示
-function showPOST($key) {
-    print getPOST($key);
+function showPOST($key, $post_data) {
+    print getPOST($key, $post_data);
 }
 
-function showPrefectures() {
+function showPrefectures($post_data) {
     global $PREFECTURES;
     $name = 'prefecture';
-    $value = getPOST($name);
+    $value = getPOST($name, $post_data);
     if (empty($value)) $value = '--';
 
     printf("<select name='%s' id='%s' size=1 value='%s'>", $name, $name, $value);
@@ -175,7 +171,7 @@ function showPrefectures() {
     print '</select>';
 }
 
-function showHobbys() {
+function showHobbys($post_data) {
     global $HOBBYS;
 
     foreach ($HOBBYS as $key => $elm) {
@@ -187,25 +183,24 @@ function showHobbys() {
         printf("<input type='checkbox' id='%s' name='hobby[]' value='%s' %s><label for='%s'>%s</label>",
                       $key, $elm, $checked, $key, $elm);
     }
-    printf("<input type='text' name='other_descript' id='other_descript value='%s'>", getPOST('other_descript'));
+    printf("<input type='text' name='other_descript' id='other_descript value='%s'>", getPOST('other_descript', $post_data));
 }
 
-function writeHiddenParams() {
+function writeHiddenParams($post_data) {
     global $NAMES;
-    global $formated_post;
 
     foreach ($NAMES as $name) {
-        $input = (isset($formated_post[$name])) ? $formated_post[$name] : '';
+        $input = (isset($post_data[$name])) ? $post_data[$name] : '';
         printf("<input type='hidden' name='%s' value='%s'>", $name, $input);
     }
-    if (isset($_POST['hobby'])) {
-        foreach ($_POST['hobby'] as $hobby) {
+    if (isset($post_data['hobby'])) {
+        foreach ($post_data['hobby'] as $hobby) {
             printf("<input type='hidden' name='hobby[]' value='%s'>", $hobby);
         }
     }
 
     //その他の趣味の詳細が入力されていたら、その他にチェックを付与
-    if (!empty($formated_post['other_descript']) && !in_array('その他', $formated_post['hobby'])) {
+    if (!empty($post_data['other_descript']) && !in_array('その他', $post_data['hobby'])) {
         print "<input type='hidden' name='hobby[]' value='その他'>";
     }
 }

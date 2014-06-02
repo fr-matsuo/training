529e58883ecef230fcc461486259312c42d6b4b6
fr-matsuo <matsuo@fact-real.com>
2014-06-02 02:15:28 +0000

    html部作成 #34

---
+++ show_form.html.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <link rel="stylesheet" type="text/css" href="common.css">
+  <title>フォーム表示</title>
+</head>
+
+<body>
+  <header>
+    <h1>フォーム表示</h1>
+  </header>
+
+  <nav>
+    <?php showFormData(); ?>
+  </nav>
+
+  <footer>
+    <p>Copyright 2014</p>
+  </footer>
+</body>	
+</html>

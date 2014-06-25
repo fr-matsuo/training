ecd64c51b38d19c3420117d89047e29a8b259bb6
fr-matsuo <matsuo@fact-real.com>
2014-06-25 02:04:12 +0000

    p要素追加処理を記述 #48

--- JS_jQuery/script.js
+++ JS_jQuery/script.js
@@ -2,3 +2,8 @@ $(function setHeaderSize() {
     var header = $("#header");
     header.css("font-size", "30px");
 });
+
+$(function addP() {
+    var sample = "<p class='sample'>サンプル2です。</p>";
+    $('#msg').after(sample);
+});

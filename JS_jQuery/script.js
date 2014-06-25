fa517ff053cdb0a38481e5d6ac4bd7f3700bad53
fr-matsuo <matsuo@fact-real.com>
2014-06-25 05:51:58 +0000

    他のコードが紛れているetcを修正 #48

--- JS_jQuery/script.js
+++ JS_jQuery/script.js
@@ -1,10 +1,8 @@
 $(function () {
-    var header = $("#header");
-    header.css("font-size", "30px");
+    $("#header").css("font-size", "30px");
 
     var sample = "<p class='sample'>サンプル2です。</p>";
     $('#msg').after(sample);
-    hover();
 
     var after = "Copyright 2013";
     $('#footer').text(after);

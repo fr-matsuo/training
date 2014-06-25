3467db1dfc70a422158345bcc7eedd5a91a0e70e
fr-matsuo <matsuo@fact-real.com>
2014-06-25 08:30:18 +0000

    アラート表示を実装 #50

---
+++ JS_jQuery/script50.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("form").submit( function () {
+        var input = $("*[name=val]").val();
+        showAlert(input);
+    });
+});
+
+function showAlert(input) {
+    if (input == "") { alert("入力してください"); }
+    else if ($.isNumeric(input)) { alert("数字です"); }
+    else { alert("数字以外です"); }
+}

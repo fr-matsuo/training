369ee17e1665fab954d57ee5cd315fb63d7090d2
fr-matsuo <matsuo@fact-real.com>
2014-06-25 05:13:45 +0000

    課題3　issue48の状態を再現 #49

---
+++ JS_jQuery/script_49_3.js
@@ -0,0 +1,10 @@
+$(function() {
+    var header = $("#header");
+    header.css("font-size", "30px");
+
+    var sample = "<p class='sample'>サンプル2です。</p>";
+    $('#msg').after(sample);
+
+    var after = "Copyright 2013";
+    $('#footer').text(after);
+});

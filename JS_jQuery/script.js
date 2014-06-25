42d9cbe5e563dfe3124c3e53a5098d73162f0fe3
fr-matsuo <matsuo@fact-real.com>
2014-06-25 02:07:46 +0000

    コピーライト変更処理を記述 #48

--- JS_jQuery/script.js
+++ JS_jQuery/script.js
@@ -7,3 +7,8 @@ $(function addP() {
     var sample = "<p class='sample'>サンプル2です。</p>";
     $('#msg').after(sample);
 });
+
+$(function changeCopyright() {
+    var after = "Copyright 2013";
+    $('#footer').text(after);
+});

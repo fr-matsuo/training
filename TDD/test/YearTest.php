210473fc7982c5819d5bfa99ec8c6e2a5998e244
fr-matsuo <matsuo@fact-real.com>
2014-07-08 08:50:56 +0000

    [Red]数字でない値をコンストラクタに渡すと例外を発生するテストを記述

--- TDD/test/YearTest.php
+++ TDD/test/YearTest.php
@@ -6,10 +6,23 @@ class YearTest extends PHPUnit_Framework_TestCase {
     public function testSetYear() {
         $this->_testSetYearModule(2000);
         $this->_testSetYearModule(2001);
-
     }
+
     private function _testSetYearModule($value) {
         $year = new Year($value);
         $this->assertEquals($year->getYear(), $value);
+    }
+
+    public function testSetIllegalYear() {
+        $this->_testIllegalYearModule('aaa');
+    }
+
+    private function _testIllegalYearModule($value) {
+        try{
+            $year = new Year($value);
+        } catch (Exception $expect) {
+            return;
+        }
+        $this->fail('asNormalYearTestIllegalYear');
     }
 }

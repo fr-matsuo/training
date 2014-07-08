dfb152875afe313a41d24008d16ab3fa056c1f06
fr-matsuo <matsuo@fact-real.com>
2014-07-08 08:12:49 +0000

    [Red]セットされた値を返すテストを、他の値で追加

--- TDD/test/YearTest.php
+++ TDD/test/YearTest.php
@@ -4,7 +4,11 @@ require_once dirname(__FILE__).'/../prod/Year.php';
 
 class YearTest extends PHPUnit_Framework_TestCase {
     public function testSetYear() {
-        $value = 2000;
+        $this->_testSetYearModule(2000);
+        $this->_testSetYearModule(2001);
+
+    }
+    private function _testSetYearModule($value) {
         $year = new Year($value);
         $this->assertEquals($year->getYear(), $value);
     }

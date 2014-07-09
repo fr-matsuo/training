cf1d4246f45983f4166e6493ef660fac13faec73
fr-matsuo <matsuo@fact-real.com>
2014-07-09 01:29:01 +0000

    [Green] 年号の表記を返す関数を実装

--- TDD/prod/Year.php
+++ TDD/prod/Year.php
@@ -3,6 +3,14 @@
 class Year {
     private $_year;
 
+    private $_jp_eras = array(
+        1868 => '明治',
+        1912 => '大正',
+        1926 => '昭和',
+        1989 => '平成',
+        2014 => 'now'   //期間内判定に現在の年が必要
+    );
+
     public  function __construct($year) {
         if (!is_numeric($year)) throw new Exception('Year is NAN...');
 
@@ -18,6 +26,25 @@ class Year {
     }
 
     public function toJpYear() {
-        return '';
+        $start_years = array_keys($this->_jp_eras);
+
+        for ($i = 0; $i < count($start_years); $i++) {
+             $start_era = $this->_jp_eras[$start_years[$i]];
+             $next_era  = $this->_jp_eras[$start_years[$i + 1]];
+
+             if ($start_years[$i]     <= $this->_year
+             &&  $start_years[$i + 1] >  $this->_year) {
+                 return $this->_createJpYear($start_era, $start_years[$i]);
+             }
+        }
+
+        return 'out of term'.$this->_year; //対象期間外
+    }
+
+    private function _createJpYear($era, $start_year) {
+        $year       = $this->_year - $start_year +1;
+        if ($year == 1) $year = '元';
+
+        return $era.$year.'年';
     }
 }

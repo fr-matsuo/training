479c76147ff664e5f7a95c784f33b39864f5cf0b
fr-matsuo <matsuo@fact-real.com>
2014-07-11 03:33:17 +0000

    [Green]テスト追加

--- TDD/test/YearTest.php
+++ TDD/test/YearTest.php
@@ -16,7 +16,11 @@ class YearTest extends PHPUnit_Framework_TestCase {
 
     public function constructYearProvider() {
         return array(
-            array(1234)
+            array(1234),
+            array(-1234),
+            array(''),
+            array('aaaaa'),
+            array(null)
         );
     }
 }

c4b9d122555ce38195f76dbf13765123ab3efa04
fr-matsuo <matsuo@fact-real.com>
2014-07-11 04:49:52 +0000

    [Green]最低限の実装

--- TDD/prod/Year.php
+++ TDD/prod/Year.php
@@ -1,13 +1,18 @@
 <?php
 
 class Year {
+    private $_is_1234;
+
     public function __construct($value) {
         if (!is_numeric($value)) {
             throw new InvalidArgumentException('NAN Year...');
         }
+
+        $this->_is_1234 = ($value == 1234);
     }
 
     public function getYear() {
-        return 1234;
+        if ($this->_is_1234) return 1234;
+        return 5678;
     }
 }

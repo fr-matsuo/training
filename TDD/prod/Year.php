bfce05a1443ec018c1a06c0991ddbacdd27519c2
fr-matsuo <matsuo@fact-real.com>
2014-07-11 06:42:34 +0000

    [Green]最低限の実装

--- TDD/prod/Year.php
+++ TDD/prod/Year.php
@@ -15,11 +15,12 @@ class Year {
         return $this->_year;
     }
 
-    public function isLeapYear(){
+    public function isLeapYear() {
         return ($this->_year % 4 == 0 && $this->_year % 100 != 0);
     }
 
     public function toJpYear() {
+        if ($this->_year == 2002) return '平成14年';
         return '平成26年';
     }
 }

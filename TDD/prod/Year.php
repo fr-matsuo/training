e1332f1297dfac061ca0a37d264a441ee825cd8b
fr-matsuo <matsuo@fact-real.com>
2014-07-11 08:41:15 +0000

    [Refactoring]元年の処理を統一

--- TDD/prod/Year.php
+++ TDD/prod/Year.php
@@ -20,11 +20,20 @@ class Year {
     }
 
     public function toJpYear() {
-        if ($this->_year == 1911) return '大正元年';
+        $first_years = array(
+            1911 => '大正',
+            1926 => '昭和',
+            1989 => '平成'
+        );
+
+        foreach ($first_years as $year => $era) {
+            if ($this->_year == $year) {
+                return $era . '元年';
+            }
+        }
+
         if ($this->_year <= 1925) return '大正'.($this->_year - 1911).'年';
-        if ($this->_year == 1926) return '昭和元年'; 
         if ($this->_year <= 1988) return '昭和'.($this->_year - 1925).'年';
-        if ($this->_year == 1989) return '平成元年';
         if ($this->_year >= 1990) return '平成'.($this->_year - 1988).'年';
     }
 }

d8f8111bc8e119c60ce949f39beba127847b1fc8
fr-matsuo <matsuo@fact-real.com>
2014-07-11 03:28:52 +0000

    [Green] Yearのコンストラクタテスト記述

--- TDD/test/YearTest.php
+++ TDD/test/YearTest.php
@@ -3,4 +3,20 @@
 require_once dirname(__FILE__).'/../prod/Year.php';
 
 class YearTest extends PHPUnit_Framework_TestCase {
+    /**
+    * @dataProvider constructYearProvider
+    */
+    public function testConstructYear($value) {
+        try {
+            $year = new Year($value);
+        } catch (Exception $error) {
+            $this->fail('Fail construct Year');
+        }
+    }
+
+    public function constructYearProvider() {
+        return array(
+            array(1234)
+        );
+    }
 }

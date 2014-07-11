69c665638360c0a0a6bb6e867a72c869cf06a7c2
fr-matsuo <matsuo@fact-real.com>
2014-07-11 07:08:12 +0000

    [Refactoring]昭和の年数を計算式から判断するように変更

--- TDD/prod/Year.php
+++ TDD/prod/Year.php
@@ -20,9 +20,9 @@ class Year {
     }
 
     public function toJpYear() {
-        if ($this->_year == 1970) return '昭和45年';
-        if ($this->_year == 1988) return '昭和63年';
+        
+        if ($this->_year <= 1988) return '昭和'.($this->_year - 1925).'年';
         if ($this->_year == 1989) return '平成元年';
-        return '平成'.($this->_year - 1988).'年';
+        if ($this->_year >= 1990) return '平成'.($this->_year - 1988).'年';
     }
 }

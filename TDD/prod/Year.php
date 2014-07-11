a056fd9422ba26ca3f08ad2e64b1b643c685d1d1
fr-matsuo <matsuo@fact-real.com>
2014-07-11 04:36:17 +0000

    [Green]最低限の実装

--- TDD/prod/Year.php
+++ TDD/prod/Year.php
@@ -2,7 +2,7 @@
 
 class Year {
     public function __construct($value) {
-        if ($value == '') {
+        if ($value == '' || $value == null || $value == 'a' || $value == array(123, 234)) {
             throw new InvalidArgumentException('NAN Year...');
         }
     }

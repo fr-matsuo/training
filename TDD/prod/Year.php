60d8254a78b3ef3fd13a7017c661da727fd946ed
fr-matsuo <matsuo@fact-real.com>
2014-07-11 04:54:35 +0000

    [Refactoring]値毎のboolの代わりに、$yearを使ってboolを生み出すように修正

--- TDD/prod/Year.php
+++ TDD/prod/Year.php
@@ -1,18 +1,18 @@
 <?php
 
 class Year {
-    private $_is_1234;
+    private $_year;
 
     public function __construct($value) {
         if (!is_numeric($value)) {
             throw new InvalidArgumentException('NAN Year...');
         }
 
-        $this->_is_1234 = ($value == 1234);
+        $this->_year = $value;
     }
 
     public function getYear() {
-        if ($this->_is_1234) return 1234;
+        if ($this->_year == 1234) return 1234;
         return 5678;
     }
 }
